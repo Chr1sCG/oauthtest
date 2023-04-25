59394a43427d26cd20047219b57e5f7530f012ed
Chr1sCG <112557454+Chr1sCG@users.noreply.github.com>
2023-04-25 17:43:20 +0200

    Update connector.config.js

--- app/connector.config.js
+++ app/connector.config.js
@@ -24,8 +24,8 @@ module.exports.connector = () => ({
     id: `notion-app`,
     name: `Notion`,
     version: config.version,
-    website: `https://notion.com`,
-    description: `More than a doc. Or a table. Customize Notion to work the way you do.`,
+    website: `https://google.com`,
+    description: `Connector description`,
     authentication: [OAuth2],
     responsibleFor: {
         dataSynchronization: true,
